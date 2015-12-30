8b97aa85b7769e19b1be2f6c1d7aaa450b53e85f
Yann Bertrand <yann@bertrand.com>
2015-12-30 15:34:40 +0100

    Generate the trailpack architecture

---
+++ index.js
@@ -0,0 +1,34 @@
+const Trailpack = require('trailpack')
+
+module.exports = class Archetype extends Trailpack {
+
+  /**
+   * TODO document method
+   */
+  validate () {
+
+  }
+
+  /**
+   * TODO document method
+   */
+  configure () {
+
+  }
+
+  /**
+   * TODO document method
+   */
+  initialize () {
+
+  }
+
+  constructor (app) {
+    super(app, {
+      config: require('./config'),
+      api: require('./api'),
+      pkg: require('./package')
+    })
+  }
+}
+
