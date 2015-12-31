2549502966766a5360aea67b4cfbbe1e92e42cec
jaumard <ioquoo3H>
2015-12-31 15:21:33 +0100

    Update Readme, Add basic usage

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
 # trailpack-gulp
 
 :package: Gulp Trailpack.
+
+## 1. Install
+```sh
+$ npm install trailpack-gulp --save
+```
+
+## 2. Configure
+
+### a. Configure Trails
+```js
+// config/main.js
+
+module.exports = {
+  ...
+  packs: [
+    ...
+    require('trailpack-gulp')
+  ]
+  ...
+}
+```
+### b. Configure Gulp
+
+This trailpack uses standard [Gulp Configuration](https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md).
+
+```js
+// config/gulp.js
+
+module.exports = {
+  defaultTaskName : 'default',
+  tasks: {
+    'default' : function (next) {
+      //Do what you want with your assets
+      next()
+    },
+    'production' : function (next) {
+      //Do what you want with your assets
+      next()
+    }
+  }
+}
+```
+
+## 3. Start!
+
+```sh
+$ npm start
+```
+## 4. Production build
+You can override the `defaultTaskName` to run like this : 
+
+```js
+// config/env/production.js
+
+module.exports = {
+  ...
+  gulp : {
+    defaultTaskName : 'production'
+  }
+  ...
+}
+```
+
+
+## License
+MIT
+
+## Maintained By
--- index.js
+++ index.js
@@ -1,29 +1,48 @@
+"use strict"
+
 const Trailpack = require('trailpack')
+const _ = require('lodash')
+const gulp = require('gulp')
 
-module.exports = class Archetype extends Trailpack {
+module.exports = class Gulp extends Trailpack {
 
   /**
-   * TODO document method
+   * Check gulp config is present
    */
-  validate () {
-
+  validate() {
+    if (!this.app.config.gulp) {
+      return Promise.reject(new Error('There no gulp.js under ./config, check it\'s load in ./config/index.js or create it!'))
+    }
+    return Promise.resolve()
   }
 
   /**
-   * TODO document method
+   * Load task to gulp
    */
-  configure () {
+  configure() {
+    const config = this.app.config.gulp
+    const tasks = config.tasks
 
+    _.each(tasks, (task, taskID) = > {
+      gulp.task(taskID, task)
+    })
+
+    return Promise.resolve()
   }
 
   /**
-   * TODO document method
+   * Run the default task
    */
-  initialize () {
-
+  initialize() {
+    return new Promise((resolve, reject) = > {
+        gulp.run(this.app.config.gulp.defaultTaskName, function (err) {
+          if (err) return reject(err)
+          resolve()
+        })
+  })
   }
 
-  constructor (app) {
+  constructor(app) {
     super(app, {
       config: require('./config'),
       api: require('./api'),
